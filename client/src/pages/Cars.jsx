16ba18c16582a8989f600d7ff8c9cc0f02f726df
JUDHI3134 <judhistirbehera456@gmail.com>
2025-07-12 11:37:16 +0530

    add some section functionality

--- client/src/pages/Cars.jsx
+++ client/src/pages/Cars.jsx
@@ -1,11 +1,69 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from '../components/Title'
 import { assets, dummyCarData } from '../assets/assets'
 import CarCard from '../components/CarCard'
+import { useSearchParams } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast'
 
 const Cars = () => {
 
+  //getting search params from url
+  const [searchParams] = useSearchParams();
+  const pickupLocation = searchParams.get('pickupLocation');
+  const pickupDate = searchParams.get('pickupDate');
+  const returnDate = searchParams.get('returnDate');
+
+  const { cars, axios } = useAppContext();
+
+  const isSearchData = pickupLocation && pickupDate && returnDate
+  const [filteredCars, setFilteredCars] = useState([])
+
   const [input, setInput] = useState("")
+
+  const applyFilter = async () => {
+    if (input === '') {
+      setFilteredCars(cars);
+      return null;
+    }
+
+    const filtered = cars.slice().filter((car) => {
+      return car.brand.toLowerCase().includes(input.toLowerCase())
+      || car.model.toLowerCase().includes(input.toLowerCase())
+      || car.category.toLowerCase().includes(input.toLowerCase())
+      || car.transmission.toLowerCase().includes(input.toLowerCase())
+    })
+
+    setFilteredCars(filtered)
+
+  }
+
+  const searchAvailability = async () => {
+    try {
+      const { data } = await axios.post("/api/bookings/check-availability", { location: pickupLocation, pickupDate, returnDate });
+
+      if (data.success) {
+        setFilteredCars(data.availableCars);
+        if (data.availableCars.length === 0) {
+          toast("No car available")
+        }
+        return null
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    cars.length > 0 && !isSearchData && applyFilter();
+  },[input, cars])
+
+
+  useEffect(() => {
+    isSearchData && searchAvailability();
+  },[])
+
+  
 
   return (
     <div>
@@ -16,7 +74,7 @@ const Cars = () => {
 
         <div className='flex items-center bg-white px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow'>
           <img alt="" src={assets.search_icon} className='w-4.5 h-4.5 mr-2' />
-          <input onClick={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Search by make, model and feature' className='w-full h-full outline-none text-gray-500' />
+          <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Search by make, model and feature' className='w-full h-full outline-none text-gray-500' />
             <img alt="" src={assets.filter_icon} className='w-4.5 h-4.5 ml-2'/>
         </div>
 
@@ -25,10 +83,10 @@ const Cars = () => {
 
       {/* ------------------------------- */}
       <div className='mt-10 px-6 md:px-16 lg:px-24 xl:px-32'>
-        <p className='text-gray-500 xl:px-20 max-w-7xl mx-auto'>Showing {dummyCarData.length} Cars</p>
+        <p className='text-gray-500 xl:px-20 max-w-7xl mx-auto'>Showing {filteredCars.length} Cars</p>
         
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 xl:px-20 max-w-7xl mx-auto'>
-          {dummyCarData.map((car, index) => (
+          {filteredCars.map((car, index) => (
             <div key={index}>
               <CarCard car={car} />
             </div>
