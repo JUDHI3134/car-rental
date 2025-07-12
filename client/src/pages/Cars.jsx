f7b7b598303b1a2fe07143372ff488bb2e6fac2e
JUDHI3134 <judhistirbehera456@gmail.com>
2025-07-12 15:18:09 +0530

    add motion animation

--- client/src/pages/Cars.jsx
+++ client/src/pages/Cars.jsx
@@ -5,6 +5,7 @@ import CarCard from '../components/CarCard'
 import { useSearchParams } from 'react-router-dom'
 import { useAppContext } from '../context/AppContext'
 import toast from 'react-hot-toast'
+import {motion} from "motion/react"
 
 const Cars = () => {
 
@@ -69,31 +70,47 @@ const Cars = () => {
     <div>
 
       {/* ---------------------------------------- */}
-      <div className='flex flex-col items-center py-20 bg-light max-md:py-4'>
+      <motion.div
+       initial={{ y: 30, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+        className='flex flex-col items-center py-20 bg-light max-md:py-4'>
         <Title title="Available Cars" subTitle={"Browse our selection of premium vehicle available for your next Adventure"} />
 
-        <div className='flex items-center bg-white px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow'>
+        <motion.div
+         initial={{ y: 20, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+          className='flex items-center bg-white px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow'>
           <img alt="" src={assets.search_icon} className='w-4.5 h-4.5 mr-2' />
           <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Search by make, model and feature' className='w-full h-full outline-none text-gray-500' />
             <img alt="" src={assets.filter_icon} className='w-4.5 h-4.5 ml-2'/>
-        </div>
+        </motion.div>
 
-      </div>
+      </motion.div>
 
 
       {/* ------------------------------- */}
-      <div className='mt-10 px-6 md:px-16 lg:px-24 xl:px-32'>
+      <motion.div
+       initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+        className='mt-10 px-6 md:px-16 lg:px-24 xl:px-32'>
         <p className='text-gray-500 xl:px-20 max-w-7xl mx-auto'>Showing {filteredCars.length} Cars</p>
         
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 xl:px-20 max-w-7xl mx-auto'>
           {filteredCars.map((car, index) => (
-            <div key={index}>
+            <motion.div
+             initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.4, delay: 0.1*index }}
+              key={index}>
               <CarCard car={car} />
-            </div>
+            </motion.div>
             ))}
         </div>
 
-      </div>
+      </motion.div>
 
 
     </div>
